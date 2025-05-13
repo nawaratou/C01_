ff34421418d2f3cacae940c3e62ea42dda8df251
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-13 18:36:42 +0100

    first commit

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-read dossier
-nb_fichiers=$(ls "$dossier" 2>/dev/null | wc -l)
-echo "Le dossier $dossier contient $nb_fichiers fichier(s)"
+read folder
+count=$(ls "$folder" 2>/dev/null | wc -l)
+echo "Le dossier $folder contient $count fichier(s)"
