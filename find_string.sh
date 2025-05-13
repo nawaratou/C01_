fc33011085188b5f7656b60ca3d44c2104870dde
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-14 00:57:54 +0100

    Correction find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <fichier> <chaine>"
-    exit 1
-fi
-
-if grep -q "$2" "$1"; then
-    echo "La chaîne '$2' a été trouvée dans $1."
-else
     echo "La chaîne '$2' n'a pas été trouvée dans $1."
-fi
