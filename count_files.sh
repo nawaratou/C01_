56ca94754c3fad3904e74bae631bddda86b19add
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-13 18:33:49 +0100

    first

--- count_files.sh
+++ count_files.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-read -p "Nom du dossier : " dossier
-
-if [ -d "$dossier" ]; then
-    ls "$dossier" | wc -l
-else
-    ls "$dossier" 2>/dev/null | wc -l
-fi
+read dossier
+nb_fichiers=$(ls "$dossier" 2>/dev/null | wc -l)
+echo "Le dossier $dossier contient $nb_fichiers fichier(s)"
