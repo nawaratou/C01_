9af49501889453e7080aa0176273d27b71d93df8
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-13 22:57:45 +0100

    recap

--- show_disk_usage.sh
+++ show_disk_usage.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 if [ -d "$1" ]; then
-  echo "Utilisation du disque pour le système :"
+
   df -h
 
-  echo ""
-  echo "Taille du dossier $1 :"
   du -sh "$1"
+  
 else
   echo "Veuillez entrer un dossier valide."
 fi
