2f9ccaba87f202894eb1c0f343f0c8ff0cfd83ee
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-14 00:26:21 +0100

    revoir

--- count_files.sh
+++ count_files.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-count=$(ls -1 "$1" 2>/dev/null | wc -l)
-echo "Le dossier $1 contient $count fichier(s)."
+echo "Le dossier ${1:-.} contient $(ls -1 "${1:-.}" 2>/dev/null | wc -l) fichier(s)."
