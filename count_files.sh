f8c37057656cdc431e823b90f865200ccbbf9fce
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-14 00:10:05 +0100

    repris

--- count_files.sh
+++ count_files.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Le dossier $1 contient $(ls $1 | wc -l) fichier(s)."
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <dossier>"
+    exit 1
+fi
+
+dossier="$1"
+count=$(ls -1 "$dossier" | wc -l)
+
+echo "Le dossier $dossier contient $count fichier(s)."
