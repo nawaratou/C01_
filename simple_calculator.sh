c73614b434ec23391d0d31f2a28d944ef61adbd3
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-14 01:09:47 +0100

    calcule correction

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <nombre1> <nombre2> <opérateur (+,-,*,/)>"
-    exit 1
-fi
-
-case $3 in
-    +) result=$(($1 + $2)) ;;
-    -) result=$(($1 - $2)) ;;
-    \*) result=$(($1 * $2)) ;;
-    /) result=$(($1 / $2)) ;;
-    *) echo "Opérateur invalide"; exit 1 ;;
-esac
-
-echo "Résultat : $result"
+echo $(( $1 $3 $2 ))
