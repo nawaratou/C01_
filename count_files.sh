575c7b993e4fb18c3124015787cff7f7e6a3802d
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-14 14:21:49 +0100

    count

--- count_files.sh
+++ count_files.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-
-# Script qui compte les fichiers dans un répertoire donné
-
-echo -n "Entrez le chemin du dossier : "
-read myfolder
-
-# Vérification que le dossier existe
-if [ ! -d "$myfolder" ]; then
-    echo "Erreur : Le dossier '$myfolder' n'existe pas." >&2
-    exit 1
-fi
-
-# Comptage des fichiers (exclut les dossiers)
-nb_fichiers=$(find "$myfolder" -maxdepth 1 -type f | wc -l)
-
-echo "Le dossier '$myfolder' contient $nb_fichiers fichier(s)."
+# compte le nombre de fichiers dans un répertoire 
+read d
+nb_fichiers=$(ls -1 "$d" | wc -l)
+echo "le dossier $d contient $nb_fichiers fichiers."
+#Affiche le nombre de fichiers
--- sample.sh
+++ sample.sh
@@ -1,1 +1,4 @@
 echo "hello people"
+read -p "" dossier
+
+echo "Le dossier $dossier contient $(ls -1 "$dossier" | wc -l) fichier(s)."
