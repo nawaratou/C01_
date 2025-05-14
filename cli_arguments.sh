25c6dc72d64a494748913ad07ee572915c699e02
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-14 13:01:18 +0100

    corve

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # vérifier si le nombre d'arguments est correct
-read -a args
+
 i=1
-for arg in " ${args[@]} "; do
+for arg in " $@ "; do
     echo "Arguments $i : $arg"
     ((i++))
 done 
