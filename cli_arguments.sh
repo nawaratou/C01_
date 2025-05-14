13f6ba8810f64a8a9c46d904f2be52d5a282c1b7
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-14 13:21:27 +0100

    azerty

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # vérifier si le nombre d'arguments est correct
-read -a arg
 
 i=1
 # afficher le nombre d'arguments
-for arg in "${arg[@]}"; do
-    echo "Arguments $i:$arg"
+for arg in "$@"; do
+    echo "Arguments $i : $arg"
     ((i++))
 done 
